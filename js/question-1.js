5fb9517cfdb6c4f2ab734e2a021bd116ed283597
Kiri M. Kvistnes <71313020+kirikvi@users.noreply.github.com>
2021-02-06 15:37:32 +0100

    formatting

--- js/question-1.js
+++ js/question-1.js
@@ -1,10 +1,8 @@
 const division = (a, b) => {
     return a % b;
 }
-
 // OR like this
 // const division = (a, b) => a % b;
 
 const divisionResult = division(11, 3);
-
 console.log(divisionResult);
